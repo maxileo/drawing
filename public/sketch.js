d2492da857d970df29cc479e5927626d2f1bf084
maxileo <casuneanu.stefan@yahoo.com>
2021-04-15 23:31:15 +0300

    First commit

---
+++ public/sketch.js
@@ -0,0 +1,33 @@
+var socket;
+
+function setup()
+{
+    createCanvas(900, 900);
+    background(200);
+
+    socket=io.connect('http://localhost:3000');
+    socket.on('mouse', newDrawing);
+}
+function newDrawing(data)
+{
+    noStroke();
+    fill(255);
+    circle(data.x, data.y, 10, 10);
+}
+
+function draw()
+{
+}
+function mouseDragged()
+{
+    var data=
+    {
+        x:mouseX,
+        y:mouseY
+    }
+    socket.emit('mouse', data);
+
+    noStroke();
+    fill(255);
+    circle(mouseX, mouseY, 10, 10);
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+
+var app=express();
+
+const port=process.env.port || 3000;
+var server=app.listen(port);
+
+
+app.use(express.static('public'));
+
+console.log("Hello there");
+
+var socket = require('socket.io');
+
+var io=socket(server);
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket)
+{
+    console.log("new connection: " + socket.id);
+
+    socket.on('mouse', mouseMsg);
+
+    function mouseMsg(data)
+    {
+        socket.broadcast.emit('mouse', data);
+        console.log(data);
+    }
+}
